30fade8a8c7f84ee1a2bdade9c929f0159007c89
gcsepregi <gabor.csepregi@4shards.org>
2025-07-11 21:51:41 +0100

    Refine authentication: skip `authenticate_user!` for Devise controllers to ensure proper Devise functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :authenticate_user!, unless: :devise_controller?
   before_action :update_user_session
 
   protected
