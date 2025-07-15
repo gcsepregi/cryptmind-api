f82d18df75ad22da88a4cd0a389b8aa058576871
gcsepregi <gabor.csepregi@4shards.org>
2025-07-15 23:42:12 +0100

    Sort journals by creation date in descending order in `index` action.

--- app/controllers/journals/journal_controller.rb
+++ app/controllers/journals/journal_controller.rb
@@ -5,7 +5,7 @@ module Journals
     respond_to :json
 
     def index
-      @journals = Journal.where(user_id: current_user.id)
+      @journals = Journal.where(user_id: current_user.id).order(created_at: :desc)
       render json: @journals.to_json, status: :ok
     end
 
