6b0a54b3702ba6f6b23322778cc912ae92e1e904
gcsepregi <gabor.csepregi@4shards.org>
2025-08-02 22:13:49 +0100

    seed roles and assign default admin role to pre-existing admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts "Seeding roles..."
+%w[admin user].each do |role|
+  Role.find_or_create_by!(name: role)
+end
+
+puts "Creating default admin user..."
+admin = User.find_by!(email: 'necromancer.morgath@gmail.com')
+
+admin.roles << Role.find_by(name: 'admin') unless admin.has_role?(:admin)
+
+puts "Seed completed."
