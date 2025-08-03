717996c982e249b1c3a44e0bb48b8daa49abfc8e
gcsepregi <gabor.csepregi@4shards.org>
2025-08-03 14:14:10 +0100

    namespace admin routes and add users controller with index action

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -4,7 +4,14 @@ module Admin
   class UsersController < AdminControllerBase
 
     def index
-      @user = User.all
+      @user = User
+                .left_joins(:user_sessions, :journal_entries)
+                .select(
+                  "users.*",
+                  "COUNT(DISTINCT user_sessions.id) AS sessions_count",
+                  "COUNT(DISTINCT journal_entries.id) AS journals_count"
+                )
+                .group("users.id")
       render json: @user.as_json, status: :ok
     end
 
