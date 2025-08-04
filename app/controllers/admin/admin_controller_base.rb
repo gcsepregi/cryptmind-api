cd8fa196260707a0877d95d69a4aa871173e862b
gcsepregi <gabor.csepregi@4shards.org>
2025-08-04 15:40:20 +0100

    allow sorting in `respond_index` with `order_by` and `order_direction` params

--- app/controllers/admin/admin_controller_base.rb
+++ app/controllers/admin/admin_controller_base.rb
@@ -8,8 +8,10 @@ class Admin::AdminControllerBase < ApplicationController
   def respond_index(data)
     index = params[:page_index].to_i || 0
     page_size = params[:page_size].to_i || 10
+    order = params[:order_by] || "id"
+    order_direction = params[:order_direction] || "asc"
     render json: {
-      data: data.limit(page_size).offset(index * page_size).as_json,
+      data: data.order(order.to_sym => order_direction.to_sym).limit(page_size).offset(index * page_size).as_json,
       pageIndex: index,
       pageSize: page_size,
       total: data.count
